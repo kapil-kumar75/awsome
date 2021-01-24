2bbd7016a75e23773539b1bc5a3019d33ff17a3e
kapil-kumar75 <64674722+kapil-kumar75@users.noreply.github.com>
2021-01-24 23:27:23 +0530

    Add files via upload

---
+++ firstClassFunc.js
@@ -0,0 +1,20 @@
+// function Statement , declaration
+
+function a() {
+  console.log("a called");
+}
+a();
+
+//function Expression
+
+const b = function () {
+  console.log("b called");
+};
+b();
+
+
+// Anonymous function
+
+function(){
+    
+}
